53c65e03bee6f3ec7d15a6624a851e5731c9ba9f
LeoKnox <LeoKnox@users.noreply.github.com>
2019-01-09 15:50:27 -0800

    Add files via upload

--- ninjaThree.js
+++ ninjaThree.js
@@ -1,12 +1,32 @@
 class Ninja {
-    constructor(name) {
-      this.name = name;
-      this.health = 100;
-      this.strength = 3;
-      this.speed = 3;
-      
-      sayName() {
-        console.log("Ninja super secret name-dont tell anyone!: ${this.name}");
-      }
-    }
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.strength = 3;
+    this.speed = 3;
   }
+  sayName() {
+    console.log(`Ninja super secret name-dont tell anyone!: ${this.name}`);
+  }
+  showStats() {
+    console.log(`Ninja: ${this.name} - Health: ${this.health} : Strength: ${this.strength} : Speed: ${this.speed}`);
+  }
+  drinkSake() {
+    console.log("Chug chug");
+    this.health += 10;
+  }
+}
+
+class Sensei extends Ninja {
+  constructor(name) {
+    super(name);
+    this.health = 200;
+    this.strength = 10;
+    this.speed = 10;
+    this.wisdom = 10;
+  }
+  speakWisdom() {
+    super.drinkSake();
+    console.log("What is the sound of three hands clapping?");
+  }
+}
